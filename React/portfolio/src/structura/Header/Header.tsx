f44b8f22985a18e86e88d8759089ea578198ff9e
chibancas <chibancas>
2023-03-06 00:56:26 +0100

    mustang

--- React/portfolio/src/structura/Header/Header.tsx
+++ React/portfolio/src/structura/Header/Header.tsx
@@ -6,15 +6,18 @@ import mst from './../../assets/mst.jpg'
 export const Header = () => {
   return (
     <header>
-        <figure>
-            <img src={logo} />
+        <figure id='logo'>
+          <img src={logo} />
+          <h1>Portfolio React</h1>
         </figure>
-        <img id='mst' src={mst} alt="" />
-        <h1>Portfolio React</h1>
+        <figure id="mst">
+          <img id='mst' src={mst} alt="" />
+        </figure>
+        
         <p>
-            Bienvenido a mi portfolio hecho con ReactJS. <br />
-            Me gustaría que no juzgarás de fomra negativa lo que verás a continuación <br />
-            ya que esta tectnología es muy conplicada y se pueden hacer grandes cosas con <br />
+            Bienvenido a mi portfolio hecho con ReactJS.
+            Me gustaría que no juzgarás de fomra negativa lo que verás a continuación
+            ya que esta tectnología es muy conplicada y se pueden hacer grandes cosas con
             pero al usuairo final no le resulta tan interesante aunque a nivel de diseño/programación <br />
             se pueden considerar asombrosas, al menos al nivel en el que yo me encuentro.
         </p>
