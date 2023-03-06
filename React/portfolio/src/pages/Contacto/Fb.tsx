77a84d6f10de48451f7f19df79a29f699aa9e5b4
chibancas <chibancas>
2023-03-07 00:03:29 +0100

    mama huebo

---
+++ React/portfolio/src/pages/Contacto/Fb.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const logofb = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png'
+export const FbPage = () => {
+  return (
+    <section id='fb'>
+        <img src={logofb}/>
+        <h2>
+            Contacta rápidamente.
+        </h2>
+    </section>
+  )
+}
--- React/portfolio/src/structura/Main/Main.tsx
+++ React/portfolio/src/structura/Main/Main.tsx
@@ -1,22 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import './Main.css'
 
 
 export const Main = () => {
   return (
     <main>
-        <section>
-            <article>
-                <figure>
-                    <img src="./../../assets/logo.svg" alt="logo de react" />
-                </figure>
-                <h2>
-                    Tarjeta de ejemplo
-                </h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel distinctio ex non placeat officia consectetur, minus porro voluptatibus, facere magni, tempora doloribus totam nostrum culpa voluptates saepe reiciendis animi nobis.
-                </p>
-            </article>
-        </section>
+        <Outlet/>
     </main>
   )
 }
