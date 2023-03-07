c8c248e6487c01a9f087343a12584aceb78d5ce2
chibancas <juanjosefdz2003@gmail.com>
2023-03-07 08:55:10 +0100

    portatil no

--- React/portfolio/src/structura/Header/Header.tsx
+++ React/portfolio/src/structura/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import logo  from './../../logo.svg'
-import mst from './../../assets/mst.jpg'
+import mst from './../../assets/mst1.jpg'
 
 export const Header = () => {
   return (
@@ -11,14 +11,14 @@ export const Header = () => {
           <h1>Portfolio React</h1>
         </figure>
         <figure id="mst">
-          <img id='mst' src={mst} alt="" />
+          {/* <img id='mst' src={mst} alt="" /> */}
         </figure>
         
         <p>
             Bienvenido a mi portfolio hecho con ReactJS.
             Me gustaría que no juzgarás de fomra negativa lo que verás a continuación
             ya que esta tectnología es muy conplicada y se pueden hacer grandes cosas con
-            pero al usuairo final no le resulta tan interesante aunque a nivel de diseño/programación <br />
+            pero al usuairo final no le resulta tan interesante aunque a nivel de diseño/programación
             se pueden considerar asombrosas, al menos al nivel en el que yo me encuentro.
         </p>
     </header>
