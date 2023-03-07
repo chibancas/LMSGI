2cf4c60cd42dfe433a957540ecbc7921dfad038c
chibancas <juanjosefdz2003@gmail.com>
2023-03-07 12:37:53 +0100

    material ui

--- React/06-miportfolio/src/index.tsx
+++ React/06-miportfolio/src/index.tsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
