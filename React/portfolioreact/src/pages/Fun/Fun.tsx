aaa5ede4e7271d7c49a4d2968f6fcb092736cc18
chibancas <juanjosefdz2003@gmail.com>
2023-03-19 18:41:05 +0100

    domingo tarde

--- React/portfolioreact/src/pages/Fun/Fun.tsx
+++ React/portfolioreact/src/pages/Fun/Fun.tsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import forza from "./../../assets/forza.jpg"
+import unchard from "./../../assets/uncharted.jpg"
+import tlou from "./../../assets/tlou.jpg"
+import gears from "./../../assets/gears.png"
+import gta from "./../../assets/gta.jpg"
+import cell from "./../../assets/blacklist.jpg"
+
+const games = [
+  {
+    foto: forza,
+    alt: "Foto de Forza Horizon 4"
+  },
+  {
+    foto: gears,
+    alt: "Foto de Gears 5"
+  },
+  {
+    foto: tlou,
+    alt: "Foto de The Last Of Us"
+  },
+  {
+    foto: gta,
+    alt: "Foto de GTA Online"
+  },
+  {
+    foto: unchard,
+    alt: "Foto de Uncharted 4"
+  },
+  {
+    foto: cell,
+    alt: "Foto de Splinter Cell"
+  }
+]
 
 export const Fun = () => {
   return (
     <main>
-        <h1>holas del mar</h1>
-    </main>
+      <section>
+        <h2>Compartimos aficiones</h2>
+        <p>
+          Pensaréis que no debería hablar de mis hobbies en un contexto formal como este, pero
+          yo opino lo contrario. Encontrar un trabajador que comparta las mismas aficiones
+          puede ser muy beneficioso para la satisfacción y motivación laboral.
+        </p>
+        <article id='games'>
+          <h3>
+            Videojuegos
+          </h3>
+          <p>Aquí dejo algunos de los videojuegos que más me gustan</p>
+          {
+            games.map((game) => {
+              return (
+                <figure>
+                  <img src={game.foto} alt={game.alt} />
+                  <span>{game.alt}</span>
+                </figure>
+              )
+            })
+          }
+        </article>
+      </section>
+    </main >
   )
 }
