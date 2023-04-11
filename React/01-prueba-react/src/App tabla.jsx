d19cee7fa3d2db88c96157ebafea88c118eefdb8
chibancas <juanjosefdz2003@gmail.com>
2023-04-11 10:12:49 +0200

    principio primer trimestre

--- React/01-prueba-react/src/App tabla.jsx
+++ React/01-prueba-react/src/App tabla.jsx
@@ -1,33 +1,33 @@
 import './App.css';
 import { heroe, titulo1 } from "./data/heroes";
-const App=()=>{
-  let titulo='Tabla Héroes'
-  return(
+const App = () => {
+  let titulo = 'Tabla Héroes'
+  return (
     <>
-    <h1>{titulo}</h1>
-    <table>
-      <thead>
-        <tr>
-          <td>ID</td>
-          <td>Nombre</td>
-          <td>Compañia</td>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          heroe.map((heroe) =>{
-            console.log(heroe);
-            return(
+      <h1>{titulo}</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Nombre</td>
+            <td>Compañia</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            heroe.map((heroe) => {
+              console.log(heroe);
+              return (
                 <tr>
                   <td>{heroe.id}</td>
                   <td>{heroe.nombre}</td>
                   <td>{heroe.owner}</td>
                 </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </>
   )
 };
