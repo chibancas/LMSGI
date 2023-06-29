c868d81ff8bd5286ba645b6fa3f30afd564291d2
chibancas <chibancas>
2023-06-29 15:54:35 +0200

    tocando cosas

--- React/testore/src/components/Main/Main.tsx
+++ React/testore/src/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 import { getFirestore } from 'firebase/firestore';
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { FirestoreProvider, useFirebaseApp } from 'reactfire';
 
 export const Main = () => {
   return (
